429e10e1d5d648350f98de016fdbb1d1352751aa
vsas1234567890-cmd <v.sas1234567890@gmail.com>
2026-05-27 14:40:13 +0200

    Create send_mail.php

---
+++ send_mail.php
@@ -0,0 +1,13 @@
+<?php
+$to = 'valentijn.sas@gmail.com';
+$naam = htmlspecialchars($_POST['naam'] ?? '');
+$email = htmlspecialchars($_POST['email'] ?? '');
+$tel = htmlspecialchars($_POST['tel'] ?? '');
+$datum = htmlspecialchars($_POST['datum'] ?? '');
+$dienst = htmlspecialchars($_POST['dienst'] ?? '');
+$bericht = htmlspecialchars($_POST['bericht'] ?? '');
+$subject = "Afspraak van $naam – BMR Tiel";
+$body = "Naam: $naam\nEmail: $email\nTel: $tel\nDatum: $datum\nDienst: $dienst\n\n$bericht";
+$headers = "From: noreply@bmr-tiel.nl\r\nReply-To: $email";
+echo json_encode(['ok' => mail($to,$subject,$body,$headers)]);
+?>
